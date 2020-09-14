463e9d206af3e8a4b646d7cff41a1e802bfdd137
besvova490 <besvova490@gmail.com>
2020-09-14 18:38:37 +0300

    add token for heroku

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from telebot_calendar import CallbackData
 
 
 TOKEN = '1317578331:AAEuCDPqvBDHMA68aWVuD5KdBAE92joNAqw'
-API = 'https://beetroot-flask-pj.herokuapp.com/'
+API = 'https://beetroot-flask-pj.herokuapp.com'
 
 SAVED_DATA = {'subjects': {}, 'teachers': {}, 'students': {},
               'my_schedule': [], 'is_teacher': False, 'user_id': 0,
